861298cdc2fb5165727e759a78aa141515a47dcc
belleomoraes <iolliveira.moraes@gmail.com>
2022-07-21 20:07:45 -0300

    map das perguntas

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,39 +1,14 @@
+const eachQuestion = ["Pergunta 1", "Pergunta 2", "Pergunta 3", "Pergunta 4", "Pergunta 5", "Pergunta 6", "Pergunta 7", "Pergunta 8"]
+
 export default function Questions() {
     return (
+        eachQuestion.map(question => 
         <div className="questions">
        <div className="question">
-        <span>Pergunta 1</span>
-        <ion-icon name="play-outline"></ion-icon>
-       </div>
-       <div className="question">
-        <span>Pergunta 2</span>
-        <ion-icon name="play-outline"></ion-icon>
-       </div>
-       <div className="question">
-        <span>Pergunta 3</span>
-        <ion-icon name="play-outline"></ion-icon>
-       </div>
-       <div className="question">
-        <span>Pergunta 4</span>
-        <ion-icon name="play-outline"></ion-icon>
-       </div>
-       <div className="question">
-        <span>Pergunta 5</span>
-        <ion-icon name="play-outline"></ion-icon>
-       </div>
-       <div className="question">
-        <span>Pergunta 6</span>
-        <ion-icon name="play-outline"></ion-icon>
-       </div>
-       <div className="question">
-        <span>Pergunta 7</span>
-        <ion-icon name="play-outline"></ion-icon>
-       </div>
-       <div className="question">
-        <span>Pergunta 8</span>
+        <span>{question}</span>
         <ion-icon name="play-outline"></ion-icon>
        </div>
        </div>
+        )
     )
-    
     }
